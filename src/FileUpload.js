54854c9a104ad5f6ebeb8f20283014747ca49fac
sarmad006 <sarmadjamal2002@gmail.com>
2022-11-14 16:17:51 +0500

    closeButton

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -162,9 +162,19 @@ const FileUpload = ({setToast,setErrorMessage}) => {
 
       {filename && url ?   
       (
+        <div className="flex items-center gap-x-4">
       <h1 > 
      <a className="text-blue-400" href={url}>  {url} </a>
+     
       </h1> 
+       <button
+       onClick={(e)=>setURL(false)}
+        type="button" 
+        class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-full focus:ring-2 focus:ring-gray-300 p-1 hover:bg-gray-100 inline-flex " data-dismiss-target="#toast-danger" aria-label="Close">
+           <span class="sr-only">Close</span>
+           <svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+       </button>
+       </div>
       ):
       (
       <DND setAcceptedFiles={setAcceptedFiles}   setErrorMessage={setErrorMessage} setToast={setToast}/>
